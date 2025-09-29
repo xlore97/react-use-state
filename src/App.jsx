1b52c541ad9ca875ef5e5fb69541bbe4b02f4b3e
Xlore97 <lorenzo.manetti.photo@gmail.com>
2025-09-29 14:05:33 +0200

    Struttura

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import languages from './components/languages'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="container">
+        <h1>Learn Web Development</h1>
+
+        <div className="buttons">
+
+        </div>
+        <div className="card">
+
+        </div>
+      </div>
 
     </>
   )
